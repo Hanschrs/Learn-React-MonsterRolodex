d060dd6ee0aad3b468e133c18338868bb0324346
Hans Christian Yulianto <hans_christian@windowslive.com>
2022-07-19 22:11:25 +0700

    Hardcoded list

--- src/App.js
+++ src/App.js
@@ -8,31 +8,39 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: 'Hans', lastName: 'Christian' },
-      company: 'HANSDiGi'
+      monsters: [
+        {
+          id: 1,
+          name: 'Linda'
+        },
+        {
+          id: 2,
+          name: 'Frank'
+        },
+        {
+          id: 3,
+          name: 'Jacky'
+        },
+        {
+          id: 4,
+          name: 'Andrei'
+        }
+      ]
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}.</p>
-          <button
-            onClick={() => {
-              this.setState(() => {
-                return {
-                  name: { firstName: 'Hans', lastName: 'Handsome' },
-                }
-              }, () => {
-                console.log(this.state);
-              });
-            }}
-          >
-            Change name
-          </button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
